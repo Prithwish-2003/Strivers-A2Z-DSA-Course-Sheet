24b6b446fffc3b0cda6af724bc3b1273adaf894b
Prithwish Ghosh <ghoshprithwish586@gmail.com>
2023-04-04 20:42:15 +0530

    Create Longest Subarray With Sum K (For Possitive and Zero).cpp

---
+++ Problems Of Array/Easy Problem/Longest Subarray With Sum K (For Possitive and Zero).cpp
@@ -0,0 +1,29 @@
+Problem link:
+
+
+https://www.codingninjas.com/codestudio/problems/longest-subarray-with-sum-k_6682399
+
+
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    // Write your code here
+    int left=0,right=0;
+    int maxi=0;
+    long long sum=a[0];
+    while (right < a.size()) {
+    while(left<=right&&sum>k){
+       sum-=a[left];
+        left++;
+       // right=left;
+
+    }
+    if(sum==k){
+        maxi=max(maxi,right-left+1);
+    }
+    right++;
+    if (right<a.size()) sum+=a[right];
+    
+    
+    }
+    return maxi;
+}
