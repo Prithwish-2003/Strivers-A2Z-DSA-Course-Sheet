9f8c72a57cb50a650c172611ea45d2f06cd1b6f5
Prithwish Ghosh <ghoshprithwish586@gmail.com>
2023-04-08 10:45:41 +0530

    Update Moore’s Voting Algorithm(Majority Element in array).cpp

--- Problems Of Array/Medium Problem/Moore’s Voting Algorithm(Majority Element in array).cpp
+++ Problems Of Array/Medium Problem/Moore’s Voting Algorithm(Majority Element in array).cpp
@@ -1,11 +1,4 @@
-logo
 
-169. Majority Element
-Description
-Hints
-Submissions
-Discuss
-Solution
 Given an array nums of size n, return the majority element.
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
